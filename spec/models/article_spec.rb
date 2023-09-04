a6500720c5616856998286051fc70538b01afc78
VincentLin0619 <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>
2023-09-04 17:48:05 +0800

    改寫測試: 使用let，在每個測試前執行let

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
+  let (:article) {create(:article)}
   it "number is be positive" do
-    a1 = create(:article)
-    expect(a1.title).to eq("test title")
+    expect(article.title).to eq("test title")
   end
   it "title is not empty" do
-    a1 = build(:article, title: "")
-    expect(a1).not_to be_valid
-    expect(a1.errors[:title]).to include("can't be blank")
+    article.title = ''
+    expect(article).not_to be_valid
+    expect(article.errors[:title]).to include("can't be blank")
   end
 end
