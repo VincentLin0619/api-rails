7bdfebd08f9f598f2ae7e251773dc18eab5017de
VincentLin0619 <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>
2023-09-04 16:39:01 +0800

    測試 article title 不能為空

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,11 @@ require "rails_helper"
 RSpec.describe Article, type: :model do
   it "number is be positive" do
     a1 = create(:article)
-    expect(a1.title).to eq("test title1")
+    expect(a1.title).to eq("test title")
+  end
+  it "title is not empty" do
+    a1 = build(:article, title: "")
+    expect(a1).not_to be_valid
+    expect(a1.errors[:title]).to include("can't be blank")
   end
 end
