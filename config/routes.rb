c413705a626f344b26e96ac4c3cbd41ece63954f
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-09-05 17:19:26 +0800

    測試路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get "/articles", to: "article#index"
+  # get "/articles", to: "article#index"
+  # get "/articles/:id", to: "article#show"
+  # post "/articles", to: "article#create"
+  # delete "/articles/:id", to: "article#destroy"
+  resources :articles, only: %i[index show create destroy update]
 end
--- spec/routes/article_spec.rb
+++ spec/routes/article_spec.rb
@@ -3,6 +3,11 @@ require "rails_helper"
 
 RSpec.describe Article, type: :routing do
   it "articles#index" do
-    expect(get("/articles")).to route_to("article#index")
+    aggregate_failures do
+      expect(get("/articles")).to route_to("articles#index")
+      expect(post("/articles")).to route_to("articles#create")
+      expect(delete("/articles/3")).to route_to("articles#destroy", id: "3")
+      expect(get("/articles?page[number]=3")).to route_to("articles#index", page: {number: 3})
+    end
   end
 end
